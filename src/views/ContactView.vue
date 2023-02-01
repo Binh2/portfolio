39f1d1902a1a91d6f25b2e14c10c70616158227e
Binh2 <hgqbinh2002@gmail.com>
2023-02-01 13:38:00 +0700

    Add more info to ContactView

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,6 +3,14 @@
     <h1>Contact</h1>
     <ul>
       <li>
+        <span>Phone number: </span>
+        <a href="tel:+84961792948" target="_blank">096-179-2948</a>
+      </li>
+      <li>
+        <span>Email: </span>
+        <a href="mailto:hgqbinh2002@gmail.com" target="_blank">hgqbinh2002@gmail.com</a>
+      </li>
+      <li>
         <span>Frontend Mentor: </span>
         <a href="https://www.frontendmentor.io/profile/Binh2" target="_blank">@Binh2</a>
       </li>
@@ -24,6 +32,7 @@ li {
   list-style-type: disclosure-closed;
   list-style-position: inside;
   color: var(--color-whitish);
+  margin-bottom: 5px;
 }
 li span, li a {
   color: var(--color-whitish);
